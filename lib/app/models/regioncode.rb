7a3e33e4bd8214da726a8f763318d2fa76271c85
Nick Kokkos <nkokkos@gmail.com>
2010-01-24 20:46:49 +0200

    Modied class methods to accept parameters for municipality and region lookup

--- lib/app/models/regioncode.rb
+++ lib/app/models/regioncode.rb
@@ -1,15 +1,21 @@
 class Regioncode < ActiveRecord::Base
    
-   def self.nomos
-      Regioncode.find_by_sql('select description, code from regioncodes where code_index=2')
+    def self.nomos
+      Regioncode.find_by_sql('select description, code from regioncodes where code_index = 2')
     end
     
-    def self.dhmotiko_diamerisma
-      Regioncode.find_by_sql('select description, code from regioncodes where code_index=5')
+    def self.dhmotiko_diamerisma(str)
+      str = str.to_s
+      search = str[0..1]
+      search = search + "%"
+      Regioncode.find_by_sql(["select description, code from regioncodes where code_index = 5 and code like ? ", search])
     end 
     
-    def self.dhmos_or_oikismos
-      Regioncode.find_by_sql('select description, code from regioncodes where code_index=6')
+    def self.dhmos_or_oikismos(str)
+      str = str.to_s
+      search = str[0..5]
+      search = search + "%"
+      Regioncode.find_by_sql(["select description, code from regioncodes where code_index = 6 and code like ? ", search])
     end
     
 end 
