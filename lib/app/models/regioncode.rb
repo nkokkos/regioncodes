688b78da3bca9a62d98927e95f5b5d8c19d276dc
nkokkos@gmail.com <nkokkos@gmail.com>
2010-02-02 12:19:33 +0200

    Added class method to return geographical region

--- lib/app/models/regioncode.rb
+++ lib/app/models/regioncode.rb
@@ -1,7 +1,21 @@
 class Regioncode < ActiveRecord::Base
+    
+    #added class method to return geographical region. Right now Greece is divided into 10 geographical regions
+    #( 
+    
+    def 
+       self.geografical_region(str)
+         if str.empty? 
+      	  Regioncode.find_by_sql('select description, code from regioncodes where code_index = 1')
+      else
+         str = str.to_s
+         search = str[0]  # first character of coderegion refers to geographical region
+         search = search + "%"
+         Regioncode.find_by_sql(["select description, code from regioncodes where code_index = 1 and code like ? ", search]
+      end
+    end
    
     def self.nomos(str)
-
       if str.empty? 
       	Regioncode.find_by_sql('select description, code from regioncodes where code_index = 2')
       else
@@ -10,7 +24,6 @@ class Regioncode < ActiveRecord::Base
         search = search + "%"
         Regioncode.find_by_sql(["select description, code from regioncodes where code_index = 2 and code like ? ", search]
       end
-
     end
     
     def self.dhmotiko_diamerisma(str)
